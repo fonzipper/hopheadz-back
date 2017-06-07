54e73c0db0b0a143423af21c583708bc709abf0e
Nikita Sergienko <nsergienko@aquivalabs.com>
2017-06-07 12:34:47 +0300

    Mongo DB setup

--- src/main/kotlin/com/hopheadz/Application.kt
+++ src/main/kotlin/com/hopheadz/Application.kt
@@ -3,14 +3,13 @@ package com.hopheadz
 import com.hopheadz.repository.IngredientRepository
 import com.hopheadz.util.Serializer
 import com.mongodb.MongoClient
-import com.mongodb.MongoCredential
-import com.mongodb.ServerAddress
-import org.springframework.beans.factory.annotation.Autowired
+import com.mongodb.MongoClientURI
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Component
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
@@ -22,14 +21,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @SpringBootApplication
 open class Application {
 
-    @Autowired
-    lateinit var connection: ConnectionConfig
+//    @Autowired
+//    lateinit var connection: ConnectionConfig
 
-    val addr = ServerAddress(connection.host, connection.port)
-    val credentials = MongoCredential.createCredential (connection.dbuser,
-                                                        connection.dbname,
-                                                        connection.password)
-    @Bean open fun db() = MongoClient(addr, listOf(credentials)).getDatabase("hopheadz")
+    @Bean open fun uri() = MongoClientURI(System.getenv("MONGODB_URI"))
+
+    @Bean open fun db() = MongoClient(uri()).getDatabase(System.getenv("DBNAME"))
     @Bean open fun serializer() = Serializer()
     @Bean open fun iRepo() = IngredientRepository(db(), serializer())
 }
@@ -49,11 +46,13 @@ open class WebConfig: WebMvcConfigurerAdapter(){
     }
 }
 
+@Component
 @ConfigurationProperties(prefix = "mongo.database")
-data class ConnectionConfig(val host: String,
-                            val port: Int,
-                            val dbname: String,
-                            val dbuser: String,
-                            val password: CharArray){
-
+open class ConnectionConfig {
+    lateinit var host: String
+    lateinit var port: Number
+    lateinit var dbname: String
+    lateinit var dbuser: String
+    lateinit var password: CharArray
+    lateinit var uri: String
 }
